6864aeb7f9572928157b96b464371650d6626663
Jhon Perez <jhon.perez@pragma.com.co>
2023-09-28 12:41:39 -0500

    add ejemplo de strategy en caso real JS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@
 // require('./singleton/javascript/singleton.ejemplo')
 
 // STRATEGY
-// require('./strategy/javascript/strategy')
-require('./strategy/javascript/strategy.exmaple')
+require('./strategy/javascript/strategy')
+// require('./strategy/javascript/strategy.exmaple')
--- src/strategy/javascript/strategy.exmaple.js
+++ src/strategy/javascript/strategy.exmaple.js
@@ -3,30 +3,123 @@ const beerArray = [
     name: 'Heineken',
     info: 'Una cerveza refrescante de los Países Bajos',
     country: 'Países Bajos',
-    imageUrl: 'https://ejemplo.com/imagen/heineken.jpg',
+    imageUrl:
+      'https://supertiendascomunal.com/2926-medium_default/cerveza-heineken-botella-330-ml.jpg',
   },
   {
     name: 'Guinness',
     info: 'Cerveza negra irlandesa con un sabor característico',
     country: 'Irlanda',
-    imageUrl: 'https://ejemplo.com/imagen/guinness.jpg',
+    imageUrl:
+      'https://www.avaloncinerock.com/cdn/shop/products/png-transparent-guinness-beer-stout-harp-lager-ale-beer-alcohol-by-volume-beer-flavor-removebg-preview_547x.png?v=1679788615',
   },
   {
     name: 'Corona',
     info: 'Cerveza mexicana con un toque de lima',
     country: 'México',
-    imageUrl: 'https://ejemplo.com/imagen/corona.jpg',
+    imageUrl:
+      'https://santacostilla.co/wp-content/uploads/2020/05/cerveza-corona-extra-santa-costilla.jpg',
   },
   {
     name: 'Budweiser',
     info: 'Cerveza estadounidense conocida por su sabor suave',
     country: 'Estados Unidos',
-    imageUrl: 'https://ejemplo.com/imagen/budweiser.jpg',
-  },
-  {
-    name: 'Stella Artois',
-    info: 'Cerveza belga con un sabor distintivo',
-    country: 'Bélgica',
-    imageUrl: 'https://ejemplo.com/imagen/stellaartois.jpg',
+    imageUrl:
+      'https://santacostilla.co/wp-content/uploads/2020/05/cerveza-budweiser-santa-costilla.jpg',
   },
 ]
+
+class BeerContext {
+  constructor(strategy, beerArray, element) {
+    this.strategy = strategy
+    this.beerArray = beerArray
+    this.element = element
+    this.setStrategy(strategy)
+  }
+
+  setStrategy(strategy) {
+    this.strategy = strategy
+  }
+
+  showBeer() {
+    this.strategy.showBeer(this.beerArray, this.element)
+  }
+}
+
+//  Estrategy para mostrar lista
+class ListBeerStrategy {
+  constructor() {}
+
+  showBeer(beerArray, element) {
+    element.innerHTML = beerArray.reduce((acc, beer) => {
+      return (
+        acc +
+        `
+        <div class="card">
+          <div class="card-header">
+            <h2>${beer.name}</h2>
+          </div>
+          <div class="card-body">
+            <p>${beer.country}</p>
+          </div>
+          <hr>
+      `
+      )
+    }, '')
+  }
+}
+
+//  Estrategy para mostrar en detalle
+
+class DetalleBeerStrategy {
+  showBeer(beerArray, element) {
+    element.innerHTML = beerArray.reduce((acc, beer) => {
+      return (
+        acc +
+        `
+        <div class="card">
+          <div class="card-header">
+            <h2>${beer.name}</h2>
+          </div>
+          <div class="card-body">
+            <p>${beer.info}</p>
+          </div>
+          <hr>
+      `
+      )
+    }, '')
+  }
+}
+//  Estrategy para mostrar en imagen
+class ImageBeerStrategy {
+  showBeer(beerArray, element) {
+    element.innerHTML = beerArray.reduce((acc, beer) => {
+      return (
+        acc +
+        `
+        <div class="card">
+          <div class="card-header">
+            <h2>${beer.name}</h2>
+          </div>
+          <div class="card-body">
+          <img src="${beer.imageUrl}" alt="${beer.name}"  width="10%"/>
+          </div>
+          <hr>
+      `
+      )
+    }, '')
+  }
+}
+// creamos una instancia de la estrategia
+const arrStrategy = [
+  new ListBeerStrategy(),
+  new DetalleBeerStrategy(),
+  new ImageBeerStrategy(),
+]
+
+// creamos una instancia del contexto y le pasamos la estrategia
+slcOptions.addEventListener('change', (event) => {
+  const opt = event.target.value
+  const beerContext = new BeerContext(arrStrategy[opt], beerArray, root)
+  beerContext.showBeer()
+})
